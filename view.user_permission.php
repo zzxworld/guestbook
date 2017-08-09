c17565c721682bade14edfc960c0777bc294e644
zzxworld <zzxworld@163.com>
2017-08-09 13:03:10 +0800

    添加用户权限编辑界面

--- view.user_permission.php
+++ view.user_permission.php
@@ -1,2 +1,23 @@
 <?php
 defined('VERSION') or die('deny access');
+
+$user = User::find(intval(getParam('id')));
+?>
+
+<div class="container">
+	<header class="page">
+		<h1>编辑 <?php echo $user['username'] ?> 的权限</h1>
+		<a class="btn" href="<?php echo url('user') ?>">返回</a>
+	</header>
+
+	<form action="<?php echo url('user_permission_confirm') ?>" method="post">
+		<div>
+			<?php foreach (Config::get('PERMISSION_LIST') as $code => $label) { ?>
+			<label><input type="checkbox" value="<?php echo $code ?>" /> <?php echo $label ?></label>
+			<?php } ?>
+		</div>
+		<div>
+			<button class="btn" type="submit">保存</button>	
+		</div>
+	</form>
+</div>
