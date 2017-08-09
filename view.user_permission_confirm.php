7edca461be6b6796eba94f42b35e63146eac2b80
zzxworld <zzxworld@163.com>
2017-08-09 13:42:20 +0800

    完善用户权限保存功能。

--- view.user_permission_confirm.php
+++ view.user_permission_confirm.php
@@ -6,11 +6,20 @@ if (!$_POST) {
 }
 
 $id = (int) getParam('id');
+$user = User::find($id);
+if (!$user) {
+	setFlashMessage('用户不存在');
+	redirect(url('user'));
+}
+
+
 $defaultPermissions = Config::get('PERMISSION_DEFAULT');
 $permissions = (array) getParam('permissions');
 $permissions = array_filter($permissions, function($rs) use($defaultPermissions) {
 	return !in_array($rs, $defaultPermissions);
 });
 
-User::updatePermission($permissions, $id);
+User::updatePermission($permissions, $user['id']);
 
+setFlashMessage('用户权限已保存');
+redirect(url('user'));
