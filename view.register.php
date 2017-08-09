1c11af62484bc73de149312e31d93260eb455f1c
zzxworld <zzxworld@163.com>
2017-08-09 09:46:48 +0800

    添加用户注册界面。

---
+++ view.register.php
@@ -0,0 +1,28 @@
+<?php
+defined('VERSION') or die('deny access');
+?>
+
+<div class="container">
+	<header class="page">
+		<h1>注册</h1>
+		<a href="<?php echo url('login') ?>" class="btn">登录</a>
+	</header>
+
+	<form action="<?php echo url('register_confirm') ?>" method="post">
+		<div>
+			<input type="text" name="email" placeholder="电子邮箱">
+		</div>
+		<div>
+			<input type="text" name="username" placeholder="用户名">
+		</div>
+		<div>
+			<input type="text" name="password" placeholder="登录密码">
+		</div>
+		<div>
+			<input type="text" name="password_confirm" placeholder="确认登录密码">
+		</div>
+		<div>
+			<button type="submit" class="btn">确认注册</button>	
+		</div>
+	</form>
+</div>
